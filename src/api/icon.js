b09e36dcb24c05a36a5ea8015d82950b29fcda98
zhaozhiqiang1 <34407840+zhaozhiqiang1@users.noreply.github.com>
2018-03-29 12:09:35 +0800

    Update icon.js

--- src/api/icon.js
+++ src/api/icon.js
@@ -1,10 +1,4 @@
-import axios from "axios"
 
-axios.defaults.baseURL = "http://localhost:8080";
-
-axios.interceptors.response.use((val)=>val.data);
-
-axios.defaults.withCredentials = true;
 
 //请求数据的接口
 
